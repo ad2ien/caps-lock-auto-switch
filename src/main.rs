a5d6945232fe26c25849ac7520d47c8ad5c2006c
Adrien Touzouli <contact@ad2ien.dev>
2024-01-31 11:14:44 +0100

    ✨ wrong caps lock detection

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,17 @@ use state::InitCell;
 
 static STATE: InitCell<RwLock<CapsLockAutoSwitchState>> = InitCell::new();
 
-const WRONG_CAPS_DETECTION: &str = r"[a-z]{1}[A-Z]{2,}";
+const WRONG_CAPS_DETECTION: &str = r"[a-z]{1}[A-Z]{2,} ";
 const NOT_WORD: &str = r"[^a-zA-Z]{1,}";
 
 struct CapsLockAutoSwitchState {
     input: String,
+}
+
+enum BufferStatus {
+    NothingSpecial,
+    WordFinished,
+    WrongCapsDetected,
 }
 
 fn main() {
@@ -32,8 +38,15 @@ fn callback(event: Event) {
     match event.name {
         Some(string) => {
             key_pressed(string);
-            if analyse_state() {
-                reset_buffer();
+            match analyse_state() {
+                BufferStatus::WordFinished => {
+                    reset_buffer();
+                },
+                BufferStatus::WrongCapsDetected => {
+                    wrong_case_detected();
+                },
+                _ => (),
+                
             }
         }
         None => (),
@@ -41,24 +54,37 @@ fn callback(event: Event) {
 }
 
 fn key_pressed(string_key: String) {
-    println!("key pressed: {} ", string_key);
     let wr_state = STATE.get();
     let mut w_state = wr_state.write().unwrap();
     w_state.input.push_str(&string_key);
 }
 
-fn analyse_state() -> bool {
+fn analyse_state() -> BufferStatus {
     let state = STATE.get().read().unwrap();
     println!("state: {}", state.input);
 
-    let re: Regex = Regex::new(NOT_WORD).unwrap();
-    return re.is_match(&state.input);
+    let re: Regex = Regex::new(WRONG_CAPS_DETECTION).unwrap();
+    if re.is_match(&state.input) {
+        return BufferStatus::WrongCapsDetected;
+    } else {
+        let re: Regex = Regex::new(NOT_WORD).unwrap();
+        if re.is_match(&state.input) {
+            return BufferStatus::WordFinished;
+        } else {
+            return BufferStatus::NothingSpecial;
+        }
+    }
 }
 
 fn reset_buffer() {
     println!("reset buffer");
     match STATE.get().try_write() {
         Ok(mut mut_state) => mut_state.input.clear(),
-        Err(_) => println!("Error: reset_buffer"),
+        Err(_) => println!("Lock error: reset_buffer"),
     }
+}
+
+fn wrong_case_detected() {
+    println!("wrong case detected!");
+    reset_buffer();
 }
