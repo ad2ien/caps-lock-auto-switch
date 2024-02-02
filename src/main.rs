de94bcfe507167c43d32d9191a5a0bf231007707
Adrien Touzouli <contact@ad2ien.dev>
2024-02-02 09:51:22 +0100

    ✨ correct last word if wrong caps detected

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,6 @@ fn main() {
     let state = CapsLockAutoSwitchState {
         input: String::new(),
     };
-
     STATE.set(RwLock::new(state));
 
     // This will block.
@@ -88,7 +87,7 @@ fn wrong_case_detected() {
 
     let state = STATE.get().read().unwrap();
 
-    correct_caps();
+    correct_caps(state.input.clone());
     Notification::new()
         .summary("Wrong case detected!")
         .body(format!("Are you sure about the case of this word : {}", state.input).as_str())
@@ -98,10 +97,31 @@ fn wrong_case_detected() {
         .unwrap();
 }
 
-fn correct_caps() {
-    println!("correct caps send event");
-    send(&EventType::KeyPress(Key::CapsLock));
-    send(&EventType::KeyRelease(Key::CapsLock));
+fn correct_caps(problematic_word: String) {
+    println!("correct caps send event. problematic_word: {}", problematic_word);
+
+    let mut first = true;
+
+    problematic_word.clone().chars().for_each(|_| {
+        send(&EventType::KeyPress(Key::Backspace));
+        send(&EventType::KeyRelease(Key::Backspace));
+    });
+
+    problematic_word.clone().chars().for_each(|c| {
+        let key = char_to_key(c);
+        if key == Key::Alt {
+            println!("error finding key associated to char: {}", c);
+            return;
+        }
+        send(&EventType::KeyPress(key));
+        send(&EventType::KeyRelease(key));
+
+        if first {
+            send(&EventType::KeyPress(Key::CapsLock));
+            send(&EventType::KeyRelease(Key::CapsLock));
+            first = false;
+        }
+    });
 }
 
 fn send(event_type: &EventType) {
@@ -114,4 +134,62 @@ fn send(event_type: &EventType) {
     }
     // Let ths OS catchup (at least MacOS)
     thread::sleep(delay);
+}
+
+fn char_to_key(c: char) -> Key {
+    match c {
+        'a' => Key::KeyA,
+        'b' => Key::KeyB,
+        'c' => Key::KeyC,
+        'd' => Key::KeyD,
+        'e' => Key::KeyE,
+        'f' => Key::KeyF,
+        'g' => Key::KeyG,
+        'h' => Key::KeyH,
+        'i' => Key::KeyI,
+        'j' => Key::KeyJ,
+        'k' => Key::KeyK,
+        'l' => Key::KeyL,
+        'm' => Key::KeyM,
+        'n' => Key::KeyN,
+        'o' => Key::KeyO,
+        'p' => Key::KeyP,
+        'q' => Key::KeyQ,
+        'r' => Key::KeyR,
+        's' => Key::KeyS,
+        't' => Key::KeyT,
+        'u' => Key::KeyU,
+        'v' => Key::KeyV,
+        'w' => Key::KeyW,
+        'x' => Key::KeyX,
+        'y' => Key::KeyY,
+        'z' => Key::KeyZ,
+        'A' => Key::KeyA,
+        'B' => Key::KeyB,
+        'C' => Key::KeyC,
+        'D' => Key::KeyD,
+        'E' => Key::KeyE,
+        'F' => Key::KeyF,
+        'G' => Key::KeyG,
+        'H' => Key::KeyH,
+        'I' => Key::KeyI,
+        'J' => Key::KeyJ,
+        'K' => Key::KeyK,
+        'L' => Key::KeyL,
+        'M' => Key::KeyM,
+        'N' => Key::KeyN,
+        'O' => Key::KeyO,
+        'P' => Key::KeyP,
+        'Q' => Key::KeyQ,
+        'R' => Key::KeyR,
+        'S' => Key::KeyS,
+        'T' => Key::KeyT,
+        'U' => Key::KeyU,
+        'V' => Key::KeyV,
+        'W' => Key::KeyW,
+        'X' => Key::KeyX,
+        'Y' => Key::KeyY,
+        'Z' => Key::KeyZ,
+        _ => Key::Alt,
+    }
 }
