4d2e6685b7cede7d7fd670cfe59a09a43b12670b
Adrien Touzouli <contact@ad2ien.dev>
2024-02-23 21:58:07 +0100

    ♻️ rename debian folder

--- debian-build.sh
+++ debian-build.sh
@@ -1,56 +1,55 @@
 #!/bin/bash
 set -e
 
-PACKAGE_NAME="capslock-auto-switch"
+DEBIAN_FOLDER="debian"
 BINARY_NAME="capslock-auto-switch"
 VERSION="0.1.0"
 REVISION="1"
-PKG_NAME="${PACKAGE_NAME}_${VERSION}-${REVISION}_all"
+PKG_NAME="${BINARY_NAME}_${VERSION}-${REVISION}_all"
 
 echo "🏗️ Start building debian package..."
 
 echo "🗑️ cleaning..."
-sudo rm -rf ${PACKAGE_NAME}/usr
-sudo rm -rf ${PACKAGE_NAME}.deb
+sudo rm -rf ${DEBIAN_FOLDER}/usr
+rm -rf ${BINARY_NAME}*.deb
 
 echo "🛠️ Build package structure"
-mkdir -p ${PACKAGE_NAME}/etc/systemd/user
-mkdir -p ${PACKAGE_NAME}/usr/share/doc/${PACKAGE_NAME}
-mkdir -p ${PACKAGE_NAME}/usr/bin
+mkdir -p ${DEBIAN_FOLDER}/etc/systemd/user
+mkdir -p ${DEBIAN_FOLDER}/usr/share/doc/${BINARY_NAME}
+mkdir -p ${DEBIAN_FOLDER}/usr/bin
 
 echo "🗞️ Compress changelog..."
-gzip -c -9 ./changelog > ./capslock-auto-switch/usr/share/doc/capslock-auto-switch/changelog.gz
+gzip -c -9 ./changelog > ./${DEBIAN_FOLDER}/usr/share/doc/${BINARY_NAME}/changelog.gz
 
 echo "🖨️ Copy files..."
-cp ./LICENSE ./${PACKAGE_NAME}/usr/share/doc/${PACKAGE_NAME}/copyright
-# cp ./capslock-auto-switch.service ./capslock-auto-switch/etc/systemd/user/capslock-auto-switch.service
+cp ./LICENSE ./${DEBIAN_FOLDER}/usr/share/doc/${BINARY_NAME}/copyright
 
 echo "🧪 run tests..."
 cargo test
 echo "🦀 run build..."
 cargo build --release
 strip target/release/$BINARY_NAME
-sudo cp target/release/$BINARY_NAME $PACKAGE_NAME/usr/bin
+sudo cp target/release/$BINARY_NAME ${DEBIAN_FOLDER}/usr/bin
 
 echo "🔏 Set permissions..."
-SPECIAL_OWNER=$(ls -alF /capslock-auto-switch/etc/systemd/user | grep -Ei ' ./' | awk '{print $3}')
+SPECIAL_OWNER=$(ls -alF /${DEBIAN_FOLDER}/etc/systemd/user | grep -Ei ' ./' | awk '{print $3}')
 if [ "$SPECIAL_OWNER" != "root" ]; then
-    sudo chown -R root:root ./capslock-auto-switch/usr
-    sudo chown -R root:root ./capslock-auto-switch/etc
+    sudo chown -R root:root ./${DEBIAN_FOLDER}/usr
+    sudo chown -R root:root ./${DEBIAN_FOLDER}/etc
 fi
-sudo chmod -R 755 $PACKAGE_NAME/usr
-sudo chmod -R 755 $PACKAGE_NAME/etc
-sudo chmod -R 755 $PACKAGE_NAME/usr/share/
-sudo chmod 644 $PACKAGE_NAME/usr/share/doc/$PACKAGE_NAME/*
+sudo chmod -R 755 ./${DEBIAN_FOLDER}/usr
+sudo chmod -R 755 ./${DEBIAN_FOLDER}/etc
+sudo chmod -R 755 ./${DEBIAN_FOLDER}/usr/share/
+sudo chmod 644 ./${DEBIAN_FOLDER}/usr/share/doc/${BINARY_NAME}/*
 
 echo "📦 Build Debian package..."
-rm -f $PKG_NAME.deb
-sudo dpkg-deb --build -Z xz ./$PACKAGE_NAME
-mv $PACKAGE_NAME.deb $PKG_NAME.deb
+rm -f ${PKG_NAME}.deb
+sudo dpkg-deb --build -Z xz ./${DEBIAN_FOLDER}
+mv ${DEBIAN_FOLDER}.deb ${PKG_NAME}.deb
 
 echo "🗹 check with lintian..."
 set +e
-docker run -it -v ./$PKG_NAME.deb:/app/$PKG_NAME.deb nouchka/lintian -c /app/$PKG_NAME.deb -v
+docker run -it -v ./${PKG_NAME}.deb:/app/${PKG_NAME}.deb nouchka/lintian -c /app/${PKG_NAME}.deb -v
 
-sudo chown $SPECIAL_OWNER:$SPECIAL_OWNER ./capslock-auto-switch/etc/systemd/user
+sudo chown $SPECIAL_OWNER:$SPECIAL_OWNER ./${DEBIAN_FOLDER}/etc/systemd/user
 echo "🎉 Done! 🎉"
